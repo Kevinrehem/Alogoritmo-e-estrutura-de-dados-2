cd3a1150f3e4b4c2fddf6f2d1bf51e419d97bc04
Kevin Rehem <kevin.rehem@gmail.com>
2025-05-18 16:11:19 -0300

    Comentários e função de inicialização da arvore adicionados

--- AEDS II/Arvore Binaria de Busca/BinarySearchTree.cpp
+++ AEDS II/Arvore Binaria de Busca/BinarySearchTree.cpp
@@ -1,12 +1,21 @@
 #include<iostream>
 using namespace std;
 
-typedef int KEY;
+typedef int KEY; //define o tipo comparável da chave a ser utilizada na estrutura Node da árvore
 
+//Estrutura que armazena o valor de um nó da árvore e o próximo valor menor e maior que ele
 struct Node{
     KEY key;
     Node *left, *right;
 };
+
+//identifica um ponteiro do tipo node como  POINT para facilitar a identificação
+typedef Node* POINT;
+
+//inicializa o primeiro node de uma árvore, que terá um Node apontando para nulo
+POINT start(){
+    return NULL;
+}
 
 
 int main(){
